57dfeaf789031f5aa5ce7a0efac02ffaace25a65
Henrique Damasceno <hdsque@outlook.com>
2023-03-09 18:05:40 -0300

    Update 0002 -  Print especial(seguindo tamanho).py

--- print especial/0002 -  Print especial(seguindo tamanho).py
+++ print especial/0002 -  Print especial(seguindo tamanho).py
@@ -1,11 +1,13 @@
 """Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com escrevaanho adaptável."""
+import os
 
 def escreva(frase):
     t = int(len(frase)+4)
     print(f"{t*'='}\n{frase.center(t)}\n{t*'='}")
 
 
-escreva("Assim será de novo")
-escreva("Bom dia, senhoras e senhores!!!")
-escreva("Função com mensagem de tamanho de separador adaptável")
-escreva("abc")
+escreva("Informe a frase para colocá entre os separadores de tmaanho adptável")
+
+frase = input('Informe a frase: ')
+os.system('cls')
+escreva(frase)
